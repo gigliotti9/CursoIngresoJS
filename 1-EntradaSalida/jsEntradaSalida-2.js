2501bff4eda4b54a4b78e98a3975cd5b17ccf4a2
gigliotti9 <tuki_1997@hotmail.com>
2017-06-26 12:32:38 -0300

    CREAMOS

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,12 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
-{
-	
+{var nombre;
+    nombre=prompt("ingrese datos");
+    alert (hola);
+    
+
+
+
 }
 
