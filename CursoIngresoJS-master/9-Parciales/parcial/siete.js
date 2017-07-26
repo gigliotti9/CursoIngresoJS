518b26858b90a50f56f147e7f1e1a8a73e765b2e
gigliotti9 <tuki_1997@hotmail.com>
2017-07-26 12:18:19 -0300

    parciales

--- CursoIngresoJS-master/9-Parciales/parcial/siete.js
+++ CursoIngresoJS-master/9-Parciales/parcial/siete.js
@@ -5,27 +5,42 @@ var promedio;
 var c_notas=0;
 var notastotales;
 var varones=0;
-while(c_notas<10){
+var bandera= true;
+var min;
+
+while(c_notas<2){
     c_notas++;
     nota=prompt("ingrese la nota nro " + c_notas);
     sexo=prompt("ingrese el sexo " + c_notas);
 
-
+nota=parseInt(nota);
 
 while(!(nota>=0 & nota<=10)){
 
    nota=prompt("ingrese las notas");  
-    
-} if (sexo="m" & nota<=6){
+    nota=parseInt(nota);
+} if (sexo=="m" & nota>=6){
     varones++;
 
-}if(nota<6){
-    
+}if(bandera){
+    min=nota;
+    bandera=false;
+}if(nota<min){
+    min=nota;
+
 }
+notastotales=notastotales+nota;
 
 }
 
-promedio=notatotales/c_notas;
+promedio=notastotales/c_notas;
+alert("la nota mas baj a  " +min + "cantidad de varones con nota mayores a 6  " +varones + "el promedio es " +promedio );
+
+
+
+
+
+
 
 
 
